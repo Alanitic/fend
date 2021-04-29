0db8cbb77a5f59070e9337c4b9bcde226a7be63f
Alan Islas <alan.islas.itic@gmail.com>
2021-04-29 11:07:04 -0500

    Navbar created

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -12,11 +12,13 @@
  * JS Standard: ESlint
  *
  */
-console.log("linked");
+
 /**
  * Define Global Variables
  *
  */
+
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -25,12 +27,44 @@ console.log("linked");
  */
 
 /**
+ * Filters a NodeList of section to maake sure each element is going to be an item of navbar
+ * @param {NodeList} sec NodeList of sections to be filtered.
+ * @returns
+ */
+function filterSections(sec) {
+  const filtered = [];
+  for (let item of sec) {
+    if (item.id.includes('section')) {
+      filtered.push(item);
+    }
+  }
+  return filtered;
+}
+
+function createNavbar(sec) {
+  const parent = document.querySelector('#navbar__list');
+  const fragment = document.createDocumentFragment();
+  parent.classList = 'navbar__menu';
+  for (let item of sec) {
+    const navbarItem = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.innerText = item.dataset?.nav;
+    anchor.classList = 'menu__link';
+    navbarItem.appendChild(anchor);
+    fragment.appendChild(navbarItem);
+  }
+  parent.appendChild(fragment);
+}
+
+/**
  * End Helper Functions
  * Begin Main Functions
  *
  */
 
 // build the nav
+const filtered = filterSections(sections);
+createNavbar(filtered);
 
 // Add class 'active' to section when near top of viewport
 
